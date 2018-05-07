be82dfc951fd6035b35a95cd749eabcd1795cd7c
llopezmalo <llopezmalo@DT37833.mx.deloitte.com>
2018-05-07 15:29:09 -0500

    Agregado CRUD para PORTALBI_ROLREPORTE

---
+++ src/main/java/com/dtte/portal/obiee/administracion/ConfiguracionReportesRolController.java
@@ -0,0 +1,193 @@
+package com.dtte.portal.obiee.administracion;
+
+import java.awt.List;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dtte.portal.obiee.dao.ConfigREPORTEROLDAO;
+import com.dtte.portal.obiee.impl.ConfigREPORTEROLImpl;
+import com.dtte.portal.obiee.model.Method_Response.ResponseCodes;
+import com.dtte.portal.obiee.model.Method_Response;
+import com.dtte.portal.obiee.model.PORTALBI_CONFIGREPORTE;
+import com.dtte.portal.obiee.model.PORTALBI_CONFIGREPORTEROL;
+import com.dtte.portal.obiee.model.PORTALBI_CONFIGROL;
+
+public class ConfiguracionReportesRolController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	private ConfigREPORTEROLDAO Implementacion = new ConfigREPORTEROLImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		String actionFromWeb = request.getServletPath();
+		System.out.println("actionFromWeb: " + actionFromWeb);
+
+		if (!actionFromWeb.toLowerCase().equals("/configuracionreportesrol"))
+			throw new ServletException("Error en el llamado");
+
+		try {
+			String accion = request.getParameter("accion");
+			System.out.println("accion: " + accion);
+
+			switch (accion) {
+			case "new":
+				newForm(request, response);
+				break;
+			case "insert":
+				insert(request, response);
+				break;
+			case "delete":
+				delete(request, response);
+				break;
+			case "edit":
+				edit(request, response);
+				break;
+			case "update":
+				update(request, response);
+				break;
+			case "consultarRolPorNombre":
+				consultarRolPorNombre(request, response);
+				break;
+			case "listar":
+			default:
+				listarConfiguraciones(request, response);
+				break;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void consultarRolPorNombre(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String nombreDespliegue = request.getParameter("NOMBRE_DESPLIEGUE");
+		response.setContentType("text/html; charset=iso-8859-1");
+		PORTALBI_CONFIGREPORTEROL valor = Implementacion.ObtainRolByName(nombreDespliegue);
+		PrintWriter out = response.getWriter();
+		out.println("Esto es una consulta del objeto Rol basado en el nombre");
+		out.println("</br>");
+//		out.println("Nombre del rol: " + parametroPrueba);
+//		out.println("Desde el objeto/Nombre: " + valor.getNombre());
+//		out.println("Desde el objeto/Parametro: " + valor.getParametro());
+//		out.println("Desde el objeto/ID: " + valor.getIdConfigrol());
+
+	}
+
+	private void newForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		java.util.List<PORTALBI_CONFIGROL> roles = Implementacion.listAllConfigsRol();
+		java.util.List<PORTALBI_CONFIGREPORTE> reportes = Implementacion.listAllConfigsReporte();
+		request.setAttribute("configuracionReportes", reportes);
+		request.setAttribute("configuracionRoles", roles);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/administracion/editarReportesRol.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void listarConfiguraciones(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+		java.util.List<PORTALBI_CONFIGREPORTEROL> listaConf = Implementacion.listAllConfigsReportesRol();
+		request.setAttribute("listaConfiguracionesRoles", listaConf);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/administracion/configuracionReportesRol.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void insert(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String strOrdenDespliegue = request.getParameter("orden").length() == 0 ? "0" : request.getParameter("orden");
+		
+		long idRol = Long.parseLong(request.getParameter("idRol"));
+		long idReporte = Long.parseLong(request.getParameter("idReporte"));
+		String nombreDespliegue = request.getParameter("nombre");
+		int ordenDespliegue = Integer.parseInt(strOrdenDespliegue);
+		String paramOpcionales = request.getParameter("opcionales");
+		String paramMandatorios = request.getParameter("mandatorios");
+		String paramNulos = request.getParameter("nulos");
+		String pagina = request.getParameter("pagina");
+		
+		PORTALBI_CONFIGREPORTEROL newRolConfig = new PORTALBI_CONFIGREPORTEROL();
+		newRolConfig.setIdRol(idRol);
+		newRolConfig.setIdReporte(idReporte);
+		newRolConfig.setNombreDespliegue(nombreDespliegue);
+		newRolConfig.setOrdenDespliegue(ordenDespliegue);
+		newRolConfig.setPagina(pagina);
+		newRolConfig.setParametrosMandatorios(paramMandatorios);
+		newRolConfig.setParametrosNulos(paramNulos);
+		newRolConfig.setParametrosOpcionales(paramOpcionales);
+		
+		Method_Response result = Implementacion.insert(newRolConfig);
+		switch (result.getCode()) {
+			case SUCCESS:
+				response.sendRedirect("configuracionreportesrol?accion=listar");
+				break;
+			case DUPLICATE_REGISTER:
+			default:
+				PrintWriter out = response.getWriter();
+				out.println(result.getMessage());
+				out.println("</br>");
+				return;
+		}
+		
+		request.getRequestDispatcher("/error.jsp").forward(request, response);
+	}
+
+	private void edit(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, NumberFormatException {
+		String idRol = request.getParameter("id");
+		String idReporte = request.getParameter("idReporte");
+		
+		PORTALBI_CONFIGREPORTEROL newConfigRol = Implementacion.getConfiguration(Long.valueOf(idRol), Long.valueOf(idReporte));		
+		request.setAttribute("configuracionReporteRol", newConfigRol);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/administracion/editarReportesRol.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void update(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		long idRol = Long.parseLong(request.getParameter("idRol"));
+		long idReporte = Long.parseLong(request.getParameter("idReporte"));
+		String nombreDespliegue = request.getParameter("nombre");
+		int ordenDespliegue = Integer.parseInt(request.getParameter("orden"));
+		String paramOpcionales = request.getParameter("opcionales");
+		String paramMandatorios = request.getParameter("mandatorios");
+		String paramNulos = request.getParameter("nulos");
+		String pagina = request.getParameter("pagina");
+		
+		PORTALBI_CONFIGREPORTEROL configReporteRol = new PORTALBI_CONFIGREPORTEROL();
+		configReporteRol.setIdRol(idRol);
+		configReporteRol.setIdReporte(idReporte);
+		configReporteRol.setNombreDespliegue(nombreDespliegue);
+		configReporteRol.setOrdenDespliegue(ordenDespliegue);
+		configReporteRol.setPagina(pagina);
+		configReporteRol.setParametrosMandatorios(paramMandatorios);
+		configReporteRol.setParametrosNulos(paramNulos);
+		configReporteRol.setParametrosOpcionales(paramOpcionales);
+		boolean estatusUpdate = Implementacion.update(configReporteRol);
+		if (estatusUpdate) {
+			response.sendRedirect("configuracionreportesrol?accion=listar");
+		} else {
+			request.getRequestDispatcher("/error.jsp").forward(request, response);
+		}
+
+	}
+
+	private void delete(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String identificador = request.getParameter("id");
+		System.out.println("Parámetro a borrar " + identificador);
+		boolean estatusBorrado = Implementacion.delete(Long.valueOf(identificador));
+		System.out.println("Estatus de borrado " + estatusBorrado);
+		response.sendRedirect("configuracionreportesrol?accion=listar");
+	}	
+}
---
+++ src/main/webapp/administracion/editarReportesRol.jsp
@@ -0,0 +1,119 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+	<title>Portal OBIEE Administraci&oacute;n</title>
+</head>
+<body>
+	<center>
+		<h1>Configuraci&oacute;n de Reportes/Roles</h1>
+        <h2>
+        	<!-- <a href="new">Agregar Configuraci&oacute;n</a>
+        	&nbsp;&nbsp;&nbsp;-->
+        	<a href="configuracionreportesrol?accion=">Listar configuraciones</a>
+        	
+        </h2>
+	</center>
+    <div align="center">
+		<c:if test="${configuracionReporteRol != null}">
+			<form action=configuracionreportesrol?accion=update method="post">
+        </c:if>
+        <c:if test="${configuracionReporteRol == null}">
+			<form action="configuracionreportesrol?accion=insert" method="post">
+        </c:if>
+        <table border="1" cellpadding="5">
+            <caption>
+            	<h2>
+            		<c:if test="${configuracionReporteRol != null}">
+            			Editar configuraci&oacute;n
+            		</c:if>
+            		<c:if test="${configuracionReporteRol == null}">
+            			Agregar nueva configuraci&oacute;n
+            		</c:if>
+            	</h2>
+            </caption>        
+           	<c:if test="${configuracionReporteRol != null}">
+            	<tr>
+					<th>ID Rol: </th>
+	                <td>
+	                	<input type="text" name="idRol" size="10" value="<c:out value='${configuracionReporteRol.idRol}'/>" readonly="readonly"/>
+	                </td>
+	            </tr>
+	            <tr>
+	            	<th>ID Reporte: </th>
+	                <td>
+             	        <input type="text" name="idReporte" size="10" value="<c:out value='${configuracionReporteRol.idReporte}'/>" readonly="readonly"/>
+	                </td>
+	            </tr>
+	        </c:if>
+	        <c:if test="${configuracionReporteRol == null}">
+	        	<tr>
+					<th>Roles: </th>
+	                <td>
+					<select name="idRol" style="width:98%">
+						<c:forEach items="${configuracionRoles}" var="rol">
+							<option value="<c:out value='${rol.idConfigrol}'/>"><c:out value='${rol.nombre}'/></option>
+						</c:forEach>
+					</select>
+	                </td>
+                </tr>
+	        	<tr>
+					<th>Reportes: </th>
+	                <td>
+					<select name="idReporte" style="width:98%">
+						<c:forEach items="${configuracionReportes}" var="reporte">
+							<option value="<c:out value='${reporte.idConfigreporte}'/>"><c:out value='${reporte.nombre}'/></option>
+						</c:forEach>
+					</select>
+	                </td>
+                </tr>
+	        </c:if>
+                
+            
+            <tr>
+                <th>Nombre: </th>
+                <td>
+                	<input type="text" name="nombre" size="50" value="<c:out value='${configuracionReporteRol.nombreDespliegue}' />" />
+                </td>
+            </tr>
+            <tr>
+                <th>Orden: </th>
+                <td>
+                	<input type="number" name="orden" min="0" size="10" value="0"/>
+                </td>
+            </tr>    
+            <tr>
+                <th>Mandatorios: </th>
+                <td>
+                	<input type="text" name="mandatorios" size="50" value="<c:out value='${configuracionReporteRol.parametrosMandatorios}' />"/>
+                </td>
+            </tr>
+            <tr>
+                <th>Opcionales: </th>
+                <td>
+                	<input type="text" name="opcionales" size="50" value="<c:out value='${configuracionReporteRol.parametrosOpcionales}' />"/>
+                </td>
+            </tr>
+            <tr>
+                <th>Nulos: </th>
+                <td>
+                	<input type="text" name="nulos" size="50" value="<c:out value='${configuracionReporteRol.parametrosNulos}' />"/>
+                </td>
+            </tr>
+            <tr>
+                <th>P&aacute;gina: </th>
+                <td>
+                	<input type="text" name="pagina" size="50" value="<c:out value='${configuracionReporteRol.pagina}' />"/>
+                </td>
+            </tr>
+            <tr>
+            	<td colspan="2" align="center">
+            		<input type="submit" value="Guardar" />
+            	</td>
+            </tr>
+        </table>
+        </form>
+    </div>	
+</body>
+</html>
