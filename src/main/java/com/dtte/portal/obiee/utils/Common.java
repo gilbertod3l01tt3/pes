bcbb355e6e735b064c3cd97d2a9b905e7be17476
gilbertod3l01tt3 <37815745+gilbertod3l01tt3@users.noreply.github.com>
2018-05-07 22:54:52 -0500

    Dynamic url
    Dynamic url

--- src/main/java/com/dtte/portal/obiee/utils/Common.java
+++ src/main/java/com/dtte/portal/obiee/utils/Common.java
@@ -1,6 +1,15 @@
 package com.dtte.portal.obiee.utils;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Common {
@@ -33,7 +42,7 @@ public class Common {
 		sb.append(":");
 		sb.append(port);
 		sb.append("/analytics/saw.dll?dashboard");
-		sb.append("&Action=Navigate");
+		// sb.append("&Action=Navigate");
 		sb.append("&NQUser=");
 		sb.append(user);
 		sb.append("&NQPassword=");
@@ -41,9 +50,51 @@ public class Common {
 		return sb;
 	}
 
-	public String getDinamicURL(Map<String, Object> map) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getDinamicURL(Map<String, Object> map) throws UnsupportedEncodingException, MalformedURLException, URISyntaxException {
+		System.out.println("getDinamicURL");
+		StringBuilder urlDin = new StringBuilder();
+		// PortalPath
+		// urlDin.append("&PortalPath=" + map.get("Path") + map.get("Panel"));
+		urlDin.append("&PortalPath=");
+		String pathplain = map.get("Path").toString() + map.get("Panel").toString();
+		//String pathPanelEncoded = URLEncoder.encode(pathplain, "UTF-8");
+		urlDin.append(pathplain);
+		map.remove("Path");
+		map.remove("Panel");
+		// Page
+		if (map.containsKey("Page")) {
+			urlDin.append("&Page=");
+			// urlDin.append(URLEncoder.encode(map.get("Page").toString(), "UTF-8"));
+			urlDin.append(map.get("Page"));
+			map.remove("Page");
+		}
+		int cuentaParametros = map.size();
+		urlDin.append("&P0=");
+		urlDin.append(cuentaParametros);
+		urlDin.append("&P1=eq");
+		// Imprimimos el Map con un Iterador
+		Iterator it = map.keySet().iterator();
+		int j = 2;
+		while (it.hasNext()) {
+			String key = (String) it.next();
+			String value = (String) map.get(key);
+
+			System.out.println("Clave: " + key + " -> Valor: " + value);
+			urlDin.append("&P" + j + "=");
+			urlDin.append(key);
+			urlDin.append("&P" + (j + 1) + "=");
+			String[] vectorCheck = value.split(",");
+			if (vectorCheck.length > 0) {
+				urlDin.append(vectorCheck.length + "+");
+				String my_new_str = value.replace(",", "+");
+				urlDin.append(my_new_str);
+				j = j + 2;
+			} else {
+				urlDin.append(value);
+				j = j + 2;
+			}
+		}
+		return urlDin.toString();
 	}
 
 }
--- src/main/java/com/dtte/portal/obiee/utils/GeneraURLDinamicaController.java
+++ src/main/java/com/dtte/portal/obiee/utils/GeneraURLDinamicaController.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,8 +15,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.dtte.portal.obiee.impl.ConfigOBIIEImpl;
+import com.dtte.portal.obiee.impl.ConfigREPORTEImpl;
+import com.dtte.portal.obiee.impl.ConfigREPORTEROLImpl;
 import com.dtte.portal.obiee.impl.ConfigROLImpl;
 import com.dtte.portal.obiee.impl.RolREPORTEImpl;
+import com.dtte.portal.obiee.model.PORTALBI_CONFIGREPORTE;
+import com.dtte.portal.obiee.model.PORTALBI_CONFIGREPORTEROL;
 import com.dtte.portal.obiee.model.PORTALBI_CONFIGROL;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,6 +33,8 @@ public class GeneraURLDinamicaController extends HttpServlet {
 	private ConfigROLImpl rolImpl = new ConfigROLImpl();
 	private Common utils = new Common();
 	private ConfigOBIIEImpl configImpl = new ConfigOBIIEImpl();
+	private ConfigREPORTEROLImpl configReporteRolImpl = new ConfigREPORTEROLImpl();
+	private ConfigREPORTEImpl configReporte = new ConfigREPORTEImpl();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -56,42 +65,74 @@ public class GeneraURLDinamicaController extends HttpServlet {
 			throw new IOException("Parametros mandatorios no se encuentran en el request");
 
 		// Obteniendo el Rol a configurar
-		// String nombre = request.getParameter("rol");
 		String nombre = (String) map.get("rol");
 		System.out.println("Obteniendo configuracion: " + nombre);
 		PORTALBI_CONFIGROL rol = rolImpl.ObtainRolByName(nombre);
-		System.out.println("Obteniendo datos desde la base: " + rol.getNombre());
-		System.out.println("Obteniendo datos desde la base/parametro: " + rol.getParametro());
+		// System.out.println("Obteniendo datos desde la base: " + rol.getNombre());
+		// System.out.println("Obteniendo datos desde la base/parametro: " +
 
 		// Obteniendo los parámetros del Reporte
-		// String idreporte = request.getParameter("reporte");
 		String idreporte = (String) map.get("reporte");
 		String[] parametrosmandatorios = rolreporteImpl.getMandatoryParam(rol.getIdConfigrol(),
 				Long.valueOf(idreporte));
 		// Validar si los parámetros mandatorios se encuentran presentes
 		System.out.println("Los parametros mandatorios son: ");
 		for (int i = 0; i < parametrosmandatorios.length; i++) {
+			String[] separarValores = parametrosmandatorios[i].split("\\|");
 			System.out.println(parametrosmandatorios[i]);
-			if (!map.containsKey(parametrosmandatorios[i])) {
+			System.out.println(separarValores);
+			if (!map.containsKey(separarValores[0])) {
 				throw new IOException("Parametros mandatorios no se encuentran en el request");
 			}
 		}
 
+		// Agregando la página si existe al mapa de variables
+		PORTALBI_CONFIGREPORTEROL pagina = configReporteRolImpl.getConfiguration(rol.getIdConfigrol(),
+				Long.valueOf(idreporte));
+		if (!pagina.getPagina().equals(null)) {
+			map.put("Page", pagina.getPagina());
+		}
+
+		// Trayendo configuración de reporte, path y panel
+		PORTALBI_CONFIGREPORTE reporteExtraido = configReporte.getConfiguration(Long.valueOf(idreporte));
+		
+		map.put("Path", reporteExtraido.getPath());
+		map.put("Panel", reporteExtraido.getPanel());
+		// Limpiando el mapa
+		map.remove("rol");
+		map.remove("reporte");
+	
 		// Traer los datos de conexión del Rol
 		Map<String, String> variablesPorReporte = utils.getVariablesAndValues(rol.getParametro());
 		StringBuilder urlEstatica = utils.buildPlainURL(configImpl.ObtainValue("servidor"),
 				configImpl.ObtainValue("PUERTO"), variablesPorReporte.get("NQUser"),
 				variablesPorReporte.get("NQPassword"));
 		System.out.println("URL estatica: " + urlEstatica.toString());
-		// Limpiando el mapa
-		map.remove("rol");
-		map.remove("reporte");
-
+		String OBIEEAction = configImpl.ObtainValue("ActionOBIEE");
+		System.out.println("Action configurada: " + OBIEEAction);
+		urlEstatica.append("&Action=" + OBIEEAction);
 		// Construyendo la url dinamica
-		String urlDinamica = utils.getDinamicURL(map);
-		System.out.println("URL dinamica: " + urlDinamica);
-		urlEstatica.append(urlDinamica);
-		out.println(urlEstatica);
+		String urlDinamica;
+		try {
+			urlDinamica = utils.getDinamicURL(map);
+			System.out.println("URL dinamica: " + urlDinamica);
+			urlEstatica.append(urlDinamica);
+		} catch (URISyntaxException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		System.out.println("URL total: " + urlEstatica);
+		URL url= new URL(urlEstatica.toString());
+		String encodedString="";
+		try {
+			URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
+			 encodedString=uri.toASCIIString();
+			 System.out.println("URL total encodeada: " + encodedString);
+		} catch (URISyntaxException e) {
+			
+			e.printStackTrace();
+		}
+		out.println(encodedString);
 	}
 
 	@Override
